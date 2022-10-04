c9ff48b43ff039459d743a2bb708f1cd0317f0f1
Luana Santos <luanapsantos91@gmail.com>
2022-10-04 20:55:03 -0300

    Atualização carrinho

--- src/js/produtos.js
+++ src/js/produtos.js
@@ -1,34 +1,66 @@
 const produtos = [
-    {
-        id:0,
-        nome: "Combo Whopper",
-        preco: 32.90,
-        image: "./src/assets/combo_whopper.png"
-    },
-    {
-        id:1,
-        nome: "BK Chicken",
-        preco: 16.90,
-        image:"./src/assets/bk_chicken.png"
-    },
-    {
-        id:2,
-        nome: "Sorvete",
-        preco: 9.80,
-        image:"./src/assets/sorvete.png"
-    },
-    {
-        id:3,
-        nome: "Cheddar Duplo",
-        preco: 13.90,
-        image: "./src/assets/cheddar_duplo.png"
-    },
-    {
-        id:4,
-        nome:"Combo Whopper Rodeio",
-        preco: 38.90,
-        image:"./src/assets/whopper_rodeio.png"
-    }, 
+  {
+    id: 0,
+    nome: "Combo Whopper",
+    preco: 32.9,
+    image: "./src/assets/combo_whopper.png",
+  },
+  {
+    id: 1,
+    nome: "BK Chicken",
+    preco: 16.9,
+    image: "./src/assets/bk_chicken.png",
+  },
+  {
+    id: 2,
+    nome: "Sorvete",
+    preco: 9.8,
+    image: "./src/assets/sorvete.png",
+  },
+  {
+    id: 3,
+    nome: "Cheddar Duplo",
+    preco: 13.9,
+    image: "./src/assets/cheddar_duplo.png",
+  },
+  {
+    id: 4,
+    nome: "Combo Whopper Rodeio",
+    preco: 38.9,
+    image: "./src/assets/whopper_rodeio.png",
+  },
+  {
+    id: 5,
+    nome: "Onion Rings",
+    preco: 11.9,
+    image: (src = "././src/assets/onion_rings.png"),
+  },
 
+  {
+    id: 6,
+    nome: "BK Mix Ovomaltine",
+    preco: 9.25,
+    image: (src = "./src/assets/bk_mix.png"),
+  },
 
-]   
+  {
+    id: 7,
+    nome: "Cheddar",
+    preco: 11.9,
+    image: (src = "./src/assets/cheddar.png"),
+  },
+
+  {
+    id: 8,
+    nome: "Shake Brownie",
+    preco: 11.35,
+    image: (src = "././src/assets/shake.png"),
+  },
+
+  {
+    id: 9,
+    preco: 9.25,
+    nome: "BK Mix Oreo",
+    image: "./src/assets/mix_oreo.png",
+  }
+];   
--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,7 @@ function criarcardProduto(produto) {
   tagLi.innerHTML = `
             <img src="${produto.image}" alt="${produto.nome}">
               <h3>${produto.nome}</h3>
-              <p>${produto.preco}</p>
+              <p>${produto.preco.toFixed(2)}</p>
               <button type="button" id"${produto.id}">Adicionar</button>
 `
   return tagLi
@@ -26,44 +26,55 @@ function listarProdutos(listaProdutos) {
 
   // 3) LOOP PARA RECUPERAR CADA PRODUTO
   for (let i = 0; i < listaProdutos.length; i++) {
-    const produto = listaProdutos[i]
+
+    const produto = listaProdutos[i];
     // 4) PASSAR PARA FUNÇÃO/PARA CRIAR O CARD DE PRODUTO
     const cardMontado = criarcardProduto(produto)
     // 5) ADICIONAR ELEMENTO (LI) NA TELA => COMO FILHO DA TAG UL
     produtosLista.appendChild(cardMontado)
   }
 }
+
+//MOSTRAR NA TELA 
 listarProdutos(produtos);
 
 //CARRINHO DE COMPRAS
-let carrinho = []
+let carrinho = [];
 
 //ADICIONANDO INTERCEPTADOR NA LISTA DE PRODUTOS
-produtosLista.addEventListener("click", adicionarProdutoCarrinho)
+produtosLista.addEventListener("click", adicionarProdutoCarrinho);
+
 function adicionarProdutoCarrinho(event){
 
   //IDENTIFICANDO ELEMENTO CLICADO
-  const botao = event.target
+  const botao = event.target;
 
   //VERIFICANDO SE É UM BOTÃO
   if (botao.tagName == "BUTTON"){
-                    
+
     //RECUPERANDO ID => IDENTIFICADOR DO PRODUTO
-    const idProduto = botao.id
+    const idProduto = botao.id;
 
     //PESQUISAR SE ESSE PRODUTO É EXISTENTE NA BASE
-    const produtoFiltrado = produtos.find((produto)=> produto.id == idProduto)
+    const produtoFiltrado = produtos.find((produto) => produto.id == idProduto) 
     
     //ADICIONANDO PRODUTO NO CARRINHO
-    carrinho.push(produtoFiltrado)
+    carrinho.push(produtoFiltrado);
 
     //LISTAR NA TELA DOS PRODUTOS
-    listarProdutosCarrinho()
+    listarProdutosCarrinho();
+
+    //FUNÇÕES QUE ATUALIZA TOTAL
+    atualizarTotal();
+
   }
 }
 
 //LISTAR PRODUTOS DO CARRINHO
 function listarProdutosCarrinho(){
+
+//PARA O PRODUTO NÃO DUPLICAR AO ADD  
+listaProdutos.innerHTML = ""
 
   //PERCORRENDO PRODUTOS DO CARRINHO
  for(let i = 0; i<carrinho.length; i++){
@@ -79,11 +90,11 @@ function listarProdutosCarrinho(){
 <!--NOME/FOTO PRODUTO-->
               <div class="infoNome">
                 <img src="${produto.image}" alt="${produto.nome}" />
-                <p>Combo Whopper</p>
+                <p>${produto.nome}</p>
               </div>
               <!--PREÇO PRODUTO-->
               <div class="infoPreco">
-                <p>R$ 32.90</p>
+                <p>R$ ${produto.preco.toFixed(2)}</p>
                 <button>
                 <img src="./src/assets/lixo.png" alt="Lixo para remover produto">
                </button>
@@ -92,8 +103,75 @@ function listarProdutosCarrinho(){
   `
   listaProdutos.appendChild(tagLi)
   
-
  }
+ }
+
+
+//ATUALIZAR TOTAL CARRINHO
+function atualizarTotal(){
+
+const infoPreco = document.querySelector(".infoPreco");
+
+let total = 0;
+
+//PERCORRE OS PRODUTOS NO CARRINHO
+for(let i = 0; i < carrinho.length; i++){
+
+//RECUPERANDO CADA PRODUTO
+const produto = carrinho[i]
+total += produto.preco
+
+}
+infoPreco.innerText = `Total: R$ ${total.toFixed(2)}`
+
+}
+
+const listaProdutosCarrinho = document.querySelector(".listaProdutos")
+listaProdutosCarrinho.addEventListener("click", removerProdutoCarrinho)
+
+function removerProdutoCarrinho(event){
+  const botaoExcluir = event.target
+
+  if (botaoExcluir.tagName == "BUTTON"){
+
+    //REMOVENDO APENAS NO HTML
+    botaoExcluir.closest("li").remove()
+
+    //IDENTIFICADOR DO PRODUTO
+    const idProduto = botaoExcluir.id
+
+    //FILTRO COM FIND
+    const produtoFiltrado = produtos.find((produto) => produto.id == idProduto)
+
+    //POSIÇÃO NUMÉRICA <= CARRINHO.INDEXOF
+    carrinho.indexOf(produtoFiltrado, [])
+    
+    //SPLICE(POSIÇÃO, 1)
+    carrinho.slice(listaProdutosCarrinho, 1)
+
+    //RETIRANDO O PRODUTO DO CARRINHO
+    //LISTAR NA TELA OS PRODUTOS
+    listarProdutosCarrinho()
+    
+    //ATUALIZAR TOTAL CARRINHO
+    atualizarTotal()
+  }
+}
+
+const cancelaProdutosCarrinho = document.querySelector(".listaProdutos")
+cancelaProdutosCarrinho.removeEventListener("click", cancelarPedido, false)
+
+function cancelarPedido(event){
+
+  const botaoCancelar = event.target
+  
+  if (botaoCancelar.tagName == "BUTTON"){
+
+    botaoCancelar.closest("li").remove()
+
+    //ATUALIZAR TOTAL CARRINHO
+    atualizarTotal()
+  }
 }
 
 
